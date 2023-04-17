518e7380f296d3e6a3ccc83311f068ff22f0435d
Joaquin Urbano Serra <kimbalear@gmail.com>
2023-04-17 21:20:56 +0200

    home & about responsive

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -106,12 +106,11 @@ $templatecontext = [
     'headercontent' => $headercontent,
     'addblockbutton' => $addblockbutton,
     'imagegirlpower' => $OUTPUT->image_url('eswatini/girlPower', 'theme_ktt_boost_child'),
-    'imageone' => $OUTPUT->image_url('imgs/img14', 'theme_ktt_boost_child'),
-    'imagetwo' => $OUTPUT->image_url('imgs/img13', 'theme_ktt_boost_child'),
-    'imagethree' => $OUTPUT->image_url('imgs/img2', 'theme_ktt_boost_child'),
-    'imagefour' => $OUTPUT->image_url('imgs/img6', 'theme_ktt_boost_child'),
-    'imagefive' => $OUTPUT->image_url('imgs/img4', 'theme_ktt_boost_child'),
-    'myname' => 'Joaquin Urbano'
+    'imageone' => $OUTPUT->image_url('imgs/sldr1', 'theme_ktt_boost_child'),
+    'imagetwo' => $OUTPUT->image_url('imgs/sldr2', 'theme_ktt_boost_child'),
+    'imagethree' => $OUTPUT->image_url('imgs/sldr3', 'theme_ktt_boost_child'),
+    'imagefour' => $OUTPUT->image_url('imgs/sldr4', 'theme_ktt_boost_child'),
+    'imagefive' => $OUTPUT->image_url('imgs/sldr5', 'theme_ktt_boost_child')
 ];
 
 $link_about = __DIR__ . '/theme/ktt_boost_child/about.php';
